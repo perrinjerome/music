673dc3ba843cd689b6b9dfbaef92151a014522b8
Jérome Perrin <perrinjerome@gmail.com>
2017-09-25 07:28:19 +0900

    log system from chrome samples

--- public/js/app.js
+++ public/js/app.js
@@ -193,3 +193,13 @@ function onHashChange () {
 }
 window.addEventListener('hashchange', onHashChange)
 setTimeout(onHashChange, 1);
+
+function log() {
+      var line = Array.prototype.slice.call(arguments).map(function(argument) {
+        return typeof argument === 'string' ? argument : JSON.stringify(argument);
+      }).join(' ');
+
+      document.querySelector('#log').textContent += line + '\n';
+      console.log(Array.prototype.slice.call(arguments));
+    }
+
