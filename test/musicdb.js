c8fdbe040600b7a76dcef5a1d054056ac253713c
Jérome Perrin <perrinjerome@gmail.com>
2017-10-02 08:47:13 +0900

    tests: use a pure js indexeddb for node

--- test/musicdb.js
+++ test/musicdb.js
@@ -1,27 +1,44 @@
-import {expect} from "chai";
-//import * as nock from "nock";
-var nock = require("nock");
-import {MusicDB} from "../public/js/musicdb";
+/*globals require, global, console, window, describe, it, beforeEach */
 
-console.log(window);
+import {expect, assert, fail} from "chai";
+var nock = require("nock");
+
+global.fetch = require('node-fetch');
+global.window.indexedDB = require("fake-indexeddb");
+
+import {MusicDB} from "../public/js/musicdb";
 
 describe("Music Database", () => {
   describe("Basic test", () => {
-    var api;
-    beforeEach( () => {
-      api = nock('https://api.example.com')
-        .get('/items')
-        .reply(200, {'items': 100, 'albums': 8});
-    });
-
     it("constructor argument is the URL of beet API", () => {
       var musicdb = new MusicDB('https://api.example.com');
       assert.isObject(musicdb);
     });
+
     it("Music db can be refreshed", () => {
-      var musicdb = new MusicDB('https://api.example.com');
-      musicdb.loadDatabase().then(() => assert(api.isDone()));
+      var musicdb = new MusicDB('http://api.example.com');
+      var api = nock('http://api.example.com')
+        .filteringPath(/[\d,]/g, '')
+        .get('/stats')
+        .reply(200, {'items': 3, 'albums': 2})
+        .get('/album/')
+        .reply(200, {'albums': [
+          {id: 1, album: "album1"},
+          {id: 2, album: "album2"}
+        ]})
+        .get('/item/')
+        .reply(200, {'items': [
+          {id: 1, album_id: 1, artist: "artist1", album: "album1", title: "1"},
+          {id: 2, album_id: 1, artist: "artist1", album: "album1", title: "2"},
+          {id: 3, album_id: 1, artist: "artist1", album: "album2", title: "3"}
+        ]});
+      return musicdb.loadDatabase().then(
+        () => {
+          api.done();
+          return musicdb.countAlbums().then((albumCount) => {
+            return expect(albumCount).to.equals(2);
+          });
+        });
     });
   });
-
 });
