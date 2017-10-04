e5c1f304c31f7651098c91143232bb26ac549296
Jérome Perrin <perrinjerome@gmail.com>
2017-10-04 21:02:17 +0900

    sw: clear old caches on activate

--- public/sw.js
+++ public/sw.js
@@ -14,6 +14,22 @@
     './worker/FlacEncoder.js'
   ];
 
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
   self.addEventListener('install', function (event) {
     // Perform install steps
     event.waitUntil(
