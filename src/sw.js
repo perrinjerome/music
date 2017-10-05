ff09b7a6ad8cd2984baf1a04e5835da9abfeb7d5
Jérome Perrin <perrinjerome@gmail.com>
2017-10-06 08:22:00 +0900

    up cache version

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 /*globals self, caches, fetch, console */
 (function () {
   "use strict";
-  var CACHE_NAME = 'music-app-v3';
+  var CACHE_NAME = 'music-app-v4';
   var urlsToCache = [
     './empty.mp3',
     './',
