ae3d5e914b32b38082ddee64097bc4800a689854
Jérome Perrin <perrinjerome@gmail.com>
2017-10-09 13:57:59 +0900

    dummy change to see if new version loads

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 /*globals self, caches, fetch, console */
 (function () {
   "use strict";
-  var CACHE_NAME = 'music-app-v7';
+  var CACHE_NAME = 'music-app-v8';
   var urlsToCache = [
     './empty.mp3',
     './',
