182ede5718f7e46d7a89c4195afffb9565fd59f2
Jérome Perrin <perrinjerome@gmail.com>
2017-10-11 22:49:39 +0900

    WIP: dynamic cache name from webpack

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 /*globals self, caches, fetch, console */
 (function () {
   "use strict";
-  var CACHE_NAME = 'music-app-v9';
+  var CACHE_NAME = 'music-app-<%= BUILD %>';
   var urlsToCache = [
     './empty.mp3',
     './',
