7d428d5d8465eeafbb159b5b95d1a6a887a5de4f
Jérome Perrin <perrinjerome@gmail.com>
2017-10-30 08:28:51 +0900

    test: test database worker

--- test/databaseLoadingWorker.test.js
+++ test/databaseLoadingWorker.test.js
@@ -1,6 +1,7 @@
 /*globals require, global, console, window, describe, it, beforeEach */
 
 var nock = require('nock');
+global.Worker = require('webworker-threads').Worker;
 
 global.fetch = require('node-fetch');
 global.indexedDB = require('fake-indexeddb');
@@ -8,19 +9,17 @@ global.indexedDB = require('fake-indexeddb');
 import { databaseLoadingWorker } from '../src/databaseLoadingWorker';
 import { DatabaseLoadingMessages } from '../src/actions';
 
-describe('Service Worker', () => {
-  describe('Database control', () => {
-    test('refreshs database in background', () => {
-      // TODO
-      /*databaseLoadingWorker.postMessage({
-        action: DatabaseLoadingMessages.REFRESH_DATABASE
-      });
-      */
-      expect(1).toEqual(1);
-    });
-    test('aborts previous loading operation if still running', () => {
-      // TODO
-      expect(1).toEqual(1);
-    });
+console.log(databaseLoadingWorker);
+describe('Database Loading Worker', () => {
+  test('refreshs database in background', () => {
+    // TODO
+    //    const worker = new Worker('../src/databaseLoadingWorker');
+    worker.onmessage = jest.fn();
+    worker.postMessage({ action: DataBaseLoadingWorker.REFRESH_DATABASE });
+    return expect(worker.onmessage).toBeCalledWith(2);
+  });
+  test('aborts previous loading operation if still running', () => {
+    // TODO
+    expect(1).toEqual(1);
   });
 });
